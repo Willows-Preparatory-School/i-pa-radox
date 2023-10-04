0c2c79aadeaac1dcbcbe18670b08c3b01d80691c
pa-radox <osun2018@hotmail.com>
2023-10-04 09:38:31 -0700

    10/4 thing

---
+++ CountDown.java
@@ -0,0 +1,10 @@
+public class CountDown {
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=100;i>0;i--){
+            sum+=i;
+            System.out.println(i);
+        }
+        System.out.println(sum);
+    }
+}
